01dc0ec9a977c7ba42a0d10a9b1c3a4f47621e8d
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2020-07-14 15:51:18 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import classes from "./App.module.css";
+
+class App extends Component {
+  render() {
+    return <div className={classes.App}>hello world!</div>;
+  }
+}
+
+export default App;
